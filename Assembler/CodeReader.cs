f395dba26dd09a56cd8e0d4c9dd4e72f811701eb
rserva <rserva@udel.edu>
2014-03-02 15:23:16 -0500

    Changed REGEX

--- Assembler/CodeReader.cs
+++ Assembler/CodeReader.cs
@@ -7,8 +7,11 @@ namespace Assembler
 {
     public class CodeReader
     {
-        private static readonly Regex regex = new Regex(@"(?:\S|(?<=\\))+",
-            RegexOptions.IgnoreCase | RegexOptions.Singleline);
+        //private static readonly Regex regex = new Regex(@"(?:\S|(?<=\\))+",
+        private static readonly Regex preregex = new Regex(@"\!.*$*",
+            RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        private static readonly Regex regex = new Regex(@"\S+",
+            RegexOptions.IgnoreCase | RegexOptions.Compiled);
 
         public static ArrayList ReadAll(String fileName)
         {
@@ -17,17 +20,31 @@ namespace Assembler
                 text.Add(line);
             return text;
         }
-
+        public static ArrayList deleteComments(ArrayList input){
+            for (int i = 0; i < input.Count; i++)
+            {
+                var element = input[i] as string;
+                MatchCollection matches = preregex.Matches(element);
+                foreach (Match match in matches)
+                {
+                    input[i] = ((String)input[i]).Substring(1, match.Index-1);
+                }
+            }
+                return input;
+        }
         public static ArrayList NormalizeText(ArrayList input)
         {
+            input = deleteComments(input);
             for (int i = 0; i < input.Count; i++)
             {
                 var element = input[i] as string;
                 MatchCollection matches = regex.Matches(element);
+                ArrayList g = new ArrayList();
                 foreach (Match match in matches)
                 {
-                    input[i] = match.Captures[0] + "," + match.Captures[1];
+                    g.Add(match.Groups[0]);
                 }
+                input[i] = g;
             }
             return input;
         }
