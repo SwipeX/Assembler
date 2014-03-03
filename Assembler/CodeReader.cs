13637b8615e26b8eddd84667438988af5fe29057
rserva <rserva@udel.edu>
2014-03-03 15:19:44 -0500

    Added jump handling

--- Assembler/CodeReader.cs
+++ Assembler/CodeReader.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace Assembler
 {
@@ -41,15 +42,66 @@ namespace Assembler
                 ArrayList g = new ArrayList();
                 foreach (Match match in matches)
                 {
-                    g.Add(match.Groups[0]);
+                    g.Add(match.Captures[0].ToString().ToLower());
                 }
                 input[i] = g;
             }
             return input;
         }
-
+        public static ArrayList handleJumps(ArrayList normalizedText)
+        {
+            ArrayList temp = new ArrayList();
+            ArrayList newNormalizedText = new ArrayList();
+            Dictionary<string, int> indexes = new Dictionary<string, int>();
+            int linecount = 0;
+            for (int i = 0; i < normalizedText.Count; i++)
+            {
+                temp = normalizedText[i]as ArrayList;
+                if (temp.Count > 2)
+                {
+                    throw new SyntaxErrorException();
+                }
+                else if(temp.Count ==1){
+                    if (temp[0].Equals("nop"))
+                    {
+                        newNormalizedText.Add(temp);
+                        linecount++;
+                    }
+                    else if((temp[0] as string).EndsWith(":"))
+                    {
+                        //do jumpy stuff
+                        indexes.Add((temp[0] as string).Substring(0, (temp[0] as string).Length - 1), linecount);
+                    }
+                    else{
+                        throw new SyntaxErrorException();
+                        
+                    }
+                }
+                else if (temp.Count == 2)
+                {
+                    newNormalizedText.Add(temp);
+                    linecount++;
+                }
+            }
+            for (int i = 0; i < newNormalizedText.Count; i++)
+            {
+                if (((newNormalizedText[i] as ArrayList)[0] as string).Equals("ba") || ((newNormalizedText[i] as ArrayList)[0] as string).Equals("be")
+                    || ((newNormalizedText[i] as ArrayList)[0] as string).Equals("bl") || ((newNormalizedText[i] as ArrayList)[0] as string).Equals("bg")
+                    )
+                {
+                    foreach(KeyValuePair<string, int> pair in indexes){
+                        if (pair.Key.Equals(((newNormalizedText[i] as ArrayList)[1] as string)))
+                        {
+                            (newNormalizedText[i] as ArrayList)[1] = pair.Value;
+                        }
+                    }
+                }
+            }
+                return newNormalizedText;
+        }
         public static ArrayList getInstructions(ArrayList normalizedText)
         {
+            normalizedText = handleJumps(normalizedText);
             var instructions = new ArrayList();
             for (int index = 0; index < normalizedText.Count; index++)
             {
