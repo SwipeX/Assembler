e930b23e9feb0a09e6735378faf9fa03d065cda3
rserva <rserva@udel.edu>
2014-03-12 20:01:08 -0400

    nigs

---
+++ Assembler/ALU.cs
@@ -0,0 +1,47 @@
+﻿namespace Assembler
+{
+    internal class ALU
+    {
+        public static void Add(int value)
+        {
+            Memory.ACC += value;
+            Memory.CC = Memory.ACC == 0? 0 : ((Memory.ACC > 0) ? 1 : -1);
+        }
+
+        public static void Sub(int value)
+        {
+            Memory.ACC -= value;
+            Memory.CC = Memory.ACC == 0 ? 0 : ((Memory.ACC > 0) ? 1 : -1);
+        }
+
+        public static void Mul(int value)
+        {
+            Memory.ACC *= value;
+            Memory.CC = Memory.ACC == 0 ? 0 : ((Memory.ACC > 0) ? 1 : -1);
+        }
+
+        public static void Div(int value)
+        {
+            Memory.ACC /= value;
+            Memory.CC = Memory.ACC == 0 ? 0 : ((Memory.ACC > 0) ? 1 : -1);
+        }
+
+        public static void And(int value)
+        {
+            Memory.ACC &= value;
+            Memory.CC = Memory.ACC == 0 ? 0 : ((Memory.ACC > 0) ? 1 : -1);
+        }
+
+        public static void Or(int value)
+        {
+            Memory.ACC |= value;
+            Memory.CC = Memory.ACC == 0 ? 0 : ((Memory.ACC > 0) ? 1 : -1);
+        }
+
+        internal static void Nor()
+        {
+            Memory.ACC ^= Memory.ACC;
+            Memory.CC = Memory.ACC == 0 ? 0 : ((Memory.ACC > 0) ? 1 : -1);
+        }
+    }
+}
