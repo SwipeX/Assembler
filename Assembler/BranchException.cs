040241485f1757b27a330a1539a720260ef2a29c
rserva <rserva@udel.edu>
2014-05-07 21:20:07 -0400

    adb

---
+++ Assembler/BranchException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assembler
+{
+    class BranchException : Exception
+    {
+        
+	public BranchException()
+	{
+
+	}
+    public BranchException(string message)
+        : base(message){
+    }
+    public BranchException(string message, Exception inner)
+        : base(message, inner){
+    }
+    }
+}
